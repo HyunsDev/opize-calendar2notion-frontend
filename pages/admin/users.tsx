4fd1ab864a98d2481441910b1ab4b646f7d7c0f4
혀느현스 <hyuns@hyuns.dev>
2023-07-13 19:01:23 +0900

    fix: 빌드 오류 수정

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -22,10 +22,6 @@ import {
 import styled from 'styled-components';
 import { AdminFooter } from '../../components/pages/admin/footer';
 import { AdminHeader } from '../../components/pages/admin/header';
-import { AdminSearchUser } from '../../components/footer/user/searchUser';
-import { AdminUserInfo } from '../../components/footer/user/userInfo';
-import { AdminUserPlanUpgrade } from '../../components/footer/user/planUpgrade';
-import { AdminUserDelete } from '../../components/footer/user/userDelete';
 import { client } from '../../lib/client';
 import { toast } from 'react-toastify';
 import { Footer } from '../../components/footer';
